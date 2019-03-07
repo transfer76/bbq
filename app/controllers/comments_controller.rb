4250292ed60d7c68190bad6e60358281e60c2d0c
transfer76 <shina76@ukr.net>
2019-03-07 20:36:53 +0200

    Creat & Setup mailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,39 +2,51 @@ class CommentsController < ApplicationController
   before_action :set_event, only: [:create, :destroy]
   before_action :set_comment, only: [:destroy]
 
- def create
-   @new_comment = @event.comments.build(comment_params)
-   @new_comment.user = current_user
+  def create
+    @new_comment = @event.comments.build(comment_params)
+    @new_comment.user = current_user
 
-   if @new_comment.save
-     redirect_to @event, notice: I18n.t('controllers.comments.created')
-   else
-     render 'events/show', alert: I18n.t('controllers.comments.error')
+    if @new_comment.save
+      notify_subscribers(@event, @new_comment)
+
+      redirect_to @event, notice: I18n.t('controllers.comments.created')
+    else
+      render 'events/show', alert: I18n.t('controllers.comments.error')
+    end
+  end
+
+  def destroy
+    message = {notice: I18n.t('controllers.comments.destroyed')}
+
+    if current_user_can_edit?(@comment)
+      @comment.destroy!
+    else
+      message = {alert: I18n.t('controllers.comments.error')}
+    end
+
+    redirect_to @event, message
    end
-  end
 
- def destroy
-   message = {notice: I18n.t('controllers.comments.destroyed')}
+   private
 
-   if current_user_can_edit?(@comment)
-     @comment.destroy!
-   else
-     message = {alert: I18n.t('controllers.comments.error')}
+   def set_event
+     @event = Event.find(params[:event_id])
    end
 
-   redirect_to @event, message
-  end
+   def set_comment
+     @comment = @event.comments.find(params[:id])
+   end
 
-  private
-  def set_event
-    @event = Event.find(params[:event_id])
-  end
+   def comment_params
+     params.require(:comment).permit(:body, :user_name)
+   end
 
-  def set_comment
-    @comment = @event.comments.find(params[:id])
-  end
+   def notify_subscribers(event, comment)
+     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
 
-  def comment_params
-    params.require(:comment).permit(:body, :user_name)
-  end
+     all_emails.each do |mail|
+       EventMailer.comment(event, comment, mail).deliver_now
+     end
+   end
 end
+
