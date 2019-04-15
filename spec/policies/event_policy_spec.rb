d15067d364d2b73298069f01fec459fa38dd80ef
transfer76 <shina76@ukr.net>
2019-04-15 17:30:56 +0300

    Modified event policy spec

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -1,21 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe EventPolicy, type: :policy do
+
   let(:user) { User.new }
 
   subject { described_class }
 
-  context "When user is not logged" do
+  context "When user is not sign in" do
+    permissions :edit?, :update?, :destroy? do
+      it { is_expected.not_to permit(nil, Event) }
+    end
+
+    permissions :show? do
+      it { is_expected.to permit(nil, Event) }
+    end
+  end
+
+  context "When user is not owner of event " do
     permissions :edit?, :update?, :destroy? do
       it { is_expected.not_to permit(user, Event) }
     end
 
     permissions :show? do
-      it { is_expected. to permit(user, Event) }
+      it { is_expected.to permit(user, Event) }
     end
   end
 
-  context "When user is logged" do
+  context "When user is owner of event" do
+
     let(:event) { user.events.build }
 
     permissions :edit?, :update?, :destroy? do
