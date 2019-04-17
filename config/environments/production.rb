aed1bf392d9bae10326a23655c75456ded57b39a
transfer76 <shina76@ukr.net>
2019-04-17 23:30:28 +0300

    Modified send mail production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Rails.application.routes.default_url_options = { host: 'visitbbq.top' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -95,7 +96,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'visitbbq.top', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'visitbbq.top' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :sendmail
