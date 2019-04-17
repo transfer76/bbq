02c9ac89e9037f1d8989c8946e9e8500745d46ef
transfer76 <shina76@ukr.net>
2019-04-17 23:16:42 +0300

    Change smtp production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,18 +95,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'visitbbq.top' }
+  config.action_mailer.default_url_options = { host: 'visitbbq.top', protocol: 'https' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
 
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domian         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domian         => 'heroku.com',
+  #   :enable_starttls_auto => true
+  # }
 end
